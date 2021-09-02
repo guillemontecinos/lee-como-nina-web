0c28f98d2b73a908b0284cb72c3cc3b51f81a6b8
Guillermo Montecinos <guillermomontecinos@Wills-iMac.home>
2021-09-01 20:37:23 -0400

    first prototype

---
+++ app.js
@@ -0,0 +1,9 @@
+const colorPalette = [
+    '#90d8c0',
+    '#d8c0f0',
+    '#c0d8f0',
+    '#f0a8a8',
+    '#f0c090'
+]
+
+document.body.style.backgroundColor = colorPalette[Math.floor(5 * Math.random())]
